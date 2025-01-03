8985a8ca9cdb80d367f6fd5ea5e1c686cb78c71d
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-01-03 11:49:12 +0300

    Bölüm 32

--- TravelTripProje/Controllers/AdminController.cs
+++ TravelTripProje/Controllers/AdminController.cs
@@ -15,5 +15,18 @@ namespace TravelTripProje.Controllers
             var degerler = c.Blogs.ToList();
             return View(degerler);
         }
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p)
+        {
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
